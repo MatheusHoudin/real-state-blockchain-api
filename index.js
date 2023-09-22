1e6e7b0fd89c52713f0f6b3d6ce8b147b363f963
Matheus Gomes <f.docarmogomes@intive.com>
2023-09-22 08:31:09 -0300

    Creating first route to fetch NFT data

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const axios = require("axios");
+const express = require("express");
+const app = express()
 const { Alchemy, Network, Wallet, Utils } = require("alchemy-sdk");
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const { ethers } = require("hardhat");
 const dotenv = require("dotenv");
 const realStateNftABI = require("./abi/RealStateNFT.json");
 const realStateCoinABI = require("./abi/RealStateCoin.json");
+
+const b3 = require('./mocks/b3.json');
+const b99 = require('./mocks/brooklyn99.json');
 
 dotenv.config();
 const { API_URL, API_KEY, PRIVATE_KEY_OWNER, CONTRACT_ADDRESS, PRIVATE_KEY_BUYER } = process.env;
@@ -24,8 +30,44 @@ const buyerSigner = new ethers.Wallet(PRIVATE_KEY_BUYER, alchemyProvider);
 // Contract
 const realStateContract = new ethers.Contract(CONTRACT_ADDRESS, realStateNftABI.abi, buyerSigner);
 
-async function main() {
-  await buyCoins();
+function main() {
+
+  app.get('/nft/:id', async (req, res) => {
+    console.log(req.params);
+    const id = req.params.id;
+    const coinAddress = await realStateContract.tokenCoin(id);
+    const propertyClient = await realStateContract.propertyClient(id);
+    const tokenUri = await realStateContract.tokenURI(id);
+    console.log(tokenUri)
+    switch (tokenUri) {
+      case 'https://private-ea9709-realstatenft.apiary-mock.com/b3':
+        res.json({
+          coinAddress: coinAddress,
+          propertyClient: propertyClient,
+          tokenUri: tokenUri,
+          nftData: b3
+        });
+      case 'www.matheus.com.br':
+        res.json({
+          coinAddress: coinAddress,
+          propertyClient: propertyClient,
+          tokenUri: tokenUri,
+          nftData: b99
+        });
+    }
+  
+    /*
+          res.json({
+        coinAddress: coinAddress,
+        propertyClient: propertyClient,
+        tokenUri: tokenUri
+      })
+    */
+  })
+
+  app.listen(3000, () => {
+    console.log(`Example app listening on port ${3000}`)
+  })
 }
 
 async function buyCoins() {
@@ -54,14 +96,6 @@ async function createNewNft() {
   );
 
   console.log(nftResult);
-}
-
-async function readTokenAndPropertyClient() {
-  const coinAddress = await realStateContract.tokenCoin(0);
-  const propertyClient = await realStateContract.propertyClient(0);
-
-  console.log(coinAddress);
-  console.log(propertyClient)
 }
 
 main();
